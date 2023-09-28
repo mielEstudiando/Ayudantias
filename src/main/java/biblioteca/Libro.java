26ba204cf757a1969165c15dfb9aea94dd091e07
miel <m.ferrada09@ufromail.cl>
2023-09-28 15:04:15 -0300

    Clase libro con métodos necesarios

--- src/main/java/biblioteca/Libro.java
+++ src/main/java/biblioteca/Libro.java
@@ -14,4 +14,37 @@ public class Libro {
     protected String getEditorial() {
         return Editorial;
     }
+
+    public Libro (){
+        this.Autor = "Autor";
+        this.Editorial = "Editorial";
+        this.Nombre = "Nombre";
+    }
+
+    public Libro (String autor, String editorial, String nombre){
+        this.Autor = autor;
+        this.Editorial = editorial;
+        this.Nombre = nombre;
+    }
+
+    public void setAutor(String autor) {
+        if (Autor.equals(autor)) {
+            Autor = autor;
+            System.out.println("Autor establecido correctamente.");
+        } else System.out.println("Autor ya registrado anteriormente.");
+    }
+
+    public void setEditorial(String editorial) {
+        if (Editorial.equals(editorial)) {
+            Editorial = editorial;
+            System.out.println("Editorial establecida correctamente.");
+        } else System.out.println("Editorial ya registrada anteriormente.");
+    }
+
+    public void setNombre(String nombre) {
+        if (Nombre.equals(nombre)) {
+            Nombre = nombre;
+            System.out.println("Nombre establecido correctamente.");
+        } else System.out.println("Nombre ya registrado anteriormente.");
+    }
 }
