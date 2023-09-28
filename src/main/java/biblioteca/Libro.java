b8864609f19666e7117b185a952a75c1c3897c44
miel <m.ferrada09@ufromail.cl>
2023-09-28 14:53:08 -0300

    Creación de clases + README

---
+++ src/main/java/biblioteca/Libro.java
@@ -0,0 +1,17 @@
+package biblioteca;
+
+public class Libro {
+    private String Nombre, Autor,
+    Editorial;
+
+    protected String getNombre() {
+        return Nombre;
+    }
+    protected String getAutor() {
+        return Autor;
+    }
+
+    protected String getEditorial() {
+        return Editorial;
+    }
+}
